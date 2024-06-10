8b81b951f94b3d7e72fc0ecada0ab99e30b5c9b0
JozzyQ <159497364+JozzyQ@users.noreply.github.com>
2024-06-09 22:41:06 -0600

    trabajo concluido

--- src/scripts/in.js
+++ src/scripts/in.js
@@ -1,29 +1,32 @@
-import { obtenerTarea, guardarTarea, modificarTarea, eliminarTarea, createElement } from './index2.js'
+/*aqui exportamos todas las funciones del index2.js que son las que 
+ingresar, eliminan, modifican daton en la API*/
+
+import { obtenerTarea, guardarTarea, modificarTarea, eliminarTarea } from './index2.js'
+/*aquio declaramos estas variables que despues seran usadas dentro de la funcion
+"createElement" que es la que cuando el usuario ingresa la tarea se refleje en la lista y tambien 
+hace el llamado de la funcion "guardarTarea" que es la que hace el POST a la API*/
 let input = document.getElementById("input");
 let agregarTareas = document.getElementById("agregarTarea");
-let button2 = document.getElementById("Eliminar")
-let lista=document.getElementById("lista")
-//let eliminarlaTarea = document.getElementById("eliminar")
-//let array = data[i]
-
-
-mostrarDatos()
-
-
+let actualizarTareas = document.getElementById("actualizarTarea");
+let conteo = document.getElementById("conteo");
+mostrarDatos();
+/*Esta funcion es la que refresca los datos en el HTML mediante el get*/
 async function mostrarDatos() {
-    console.log("hola")
-
-
-
   let valor = await obtenerTarea()
-
+  document.getElementById("lista").innerHTML = "";
  valor.forEach(element => {
-
     let lista = document.getElementById("lista");
     let tareaEnLista = document.createElement("li");
     let check=document.createElement("input");
     check.type="checkbox";
-    check.id="checkid";
+    check.id=element.id;
+    check.addEventListener("click",function(){
+        if (check.checked == true) {
+            conteo.innerHTML = conteo.innerHTML+++1;
+        } else {
+            conteo.innerHTML = parseInt(conteo.innerHTML)-1;
+        }
+    })
     console.log(check.value);
     check.style.marginLeft = "50%";
     tareaEnLista.textContent = element.task;
@@ -33,69 +36,51 @@ async function mostrarDatos() {
     let button = document.createElement("button");
     button.innerHTML = "Modificar";
     button.id="Modificar";
+    button.lang=element.id;
     lista.appendChild(button);
     let button2 = document.createElement("button");
     button2.innerHTML = "Eliminar";
     button2.style.marginLeft = "2%";
     button2.id="Eliminar";
-    button2.onclick=borrar()
+    button2.lang= element.id;
+    button.addEventListener("click", function(){
+        document.getElementById("hdnid").value=button.lang;
+        document.getElementById("input").value=element.task;
+        document.getElementById("actualizarTarea").style.display="";
+        document.getElementById("agregarTarea").style.display="none";
+    })
+    
+    button2.addEventListener("click", function(){
+        
+        eliminarTarea(button2.lang);
+    })
     lista.appendChild(button2);
   });
-
-  
- 
- 
-
 }
-
-
+/*Funcion que ingresa la tarea al HTML*/
 agregarTareas.addEventListener("click", async function () {
-
+    if (input.value.trim() === "") {
+        alert("Ingrese una tarea");
+        return;
+    } 
+        guardarTarea(input.value);
+        mostrarDatos()
+        
+        
+    
+})
+/*Funcion que deja modificar cualquier tarea que se halla ingresado a la lista,
+ una vez que ud actualice la tarea se refleje en la API y tambien en el HTML */
+actualizarTareas.addEventListener("click", async function () {
     if (input.value.trim() === "") {
         alert("Ingrese una tarea");
         return;
     } else {
-        console.log("hola")
-        guardarTarea(input.value)
-        createElement()
-        let button2 = document.getElementById("Eliminar")
-        let button = document.getElementById("Modificar")
-        if (button2 == true) {
-
-             button2.addEventListener("click", function () {
-
-            
-        })
-        } else if (button == true) {
-            button, addEventListener("click", function () {
-
-              
-            })
-        }
-
-        
-       
-        
-        
+        modificarTarea(input.value,document.getElementById("hdnid").value);
+        document.getElementById("actualizarTarea").style.display="none";
+        document.getElementById("agregarTarea").style.display="";
+        input.value = "";
+        mostrarDatos();
     }
-
-
 })
 
-
-/*let button2 = document.createElement("button");
-
-
-button2.addEventListener("click", function(){
-    
-     console.log("hola") 
-      
-     for (let i = 0; i < data.length; i++) {
-        if (data.id == data[i]) {
-            eliminarTarea(data.id)
-            break
-            
-        }
-        
-    }
-})*/
--- src/scripts/index2.js
+++ src/scripts/index2.js
@@ -1,16 +1,17 @@
+/*Funcion que hace que el get se ejecute y se pueda ver la lista 
+por si se refresca la pagina */
+
+
 async function obtenerTarea() {
     try {
         const response = await fetch('http://localhost:3000/api/task/');
         const data = await response.json();
-        // console.log("aqui vamos", data)
         return data
     } catch (error) {
         console.error(error);
     }
 }
-// console.log(data)
-
-
+/*Funcion que lo que hace es ingresar la tarea a la API */
 async function guardarTarea(titulo) {
     try {
         const response = await fetch("http://localhost:3000/api/task/", {
@@ -30,7 +31,9 @@ async function guardarTarea(titulo) {
         });
         if (response.ok) {
             const data = await response.json();
+            
             return data
+            
         } else {
             console.error("Error al guardar la tarea:", response.statusText);
         }
@@ -38,15 +41,20 @@ async function guardarTarea(titulo) {
         console.error("Error al realizar la solicitud:", error);
     }
 }
-
-async function modificarTarea(titulo) {
+/*FEsta funcion nos deja modificar la tarea y claramente se actualiza en la API */
+async function modificarTarea(titulo,id) {
     try {
-        const response = await fetch("http://localhost:3000/api/task/", {
+        const response = await fetch("http://localhost:3000/api/task/"+id, {
             method: "PUT",
             headers: {
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify(titulo)
+            
+            body: JSON.stringify(
+                {
+                    task:titulo
+                }
+                )
         });
         if (response.ok) {
             const data = await response.json();
@@ -58,7 +66,7 @@ async function modificarTarea(titulo) {
     }
 }
 
-
+/*Esta funcion elimina la tera de la API*/
 async function eliminarTarea(id) {
     try {
         const response = await fetch("http://localhost:3000/api/task/"+id, {
@@ -80,60 +88,9 @@ async function eliminarTarea(id) {
     window.location.reload()
 }
 
-async function createElement() {
 
-    
-
-
-  //let valor = await obtenerTarea()
-
-
-     
-
-    
-    let lista = document.getElementById("lista");
-    let tareaEnLista = document.createElement("li");
-    let check=document.createElement("input");
-    check.type="checkbox";
-    check.id="checkid";
-    console.log(check.value);
-    check.style.marginLeft = "50%";
-    console.log("aqui vamos" , check)
-    tareaEnLista.textContent = input.value;
-   
-    lista.appendChild(tareaEnLista);
-    lista.appendChild(check);
-    let button = document.createElement("button");
-    button.innerHTML = "Modificar";
-    button.id="Modificar";
-    lista.appendChild(button);
-    let button2 = document.createElement("button");
-    button2.innerHTML = "Eliminar";
-    button2.style.marginLeft = "2%";
-    button2.id="Eliminar";
-    button2.onclick= ()=> eliminar(button2)
-    
-    function eliminar(button2){
-        const allSelectCheckboxes = document.querySelectorAll('button[name="Modificar"]');
-            allSelectCheckboxes.forEach(cb => {
-                if (cb !== button2) cb.checked = false;
-            });
-            console.log("seleccion",button2.checked)
-            const delete1 = documentQuerySelector('button[name="Modificar"]:checked'); 
-            console.log("aqui vamos", delete1)
-    }    
-    lista.appendChild(button2);
-
-    input.value= ""
-    
-
-
-    
-
-}
-
-
-export {obtenerTarea,guardarTarea,modificarTarea,eliminarTarea,createElement};
+/*Aqui modulamos exportando todas estas funciones para el in.js */
+export {obtenerTarea,guardarTarea,modificarTarea,eliminarTarea};
 
 
 
